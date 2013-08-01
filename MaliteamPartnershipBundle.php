389c8f822c94a1c26eb8987fb23b8bb92d634aff
Sekou KOITA <sictoz@gmail.com>
2013-08-01 17:38:30 +0200

    Adding MaliteamPartnershipBundle.php

---
+++ MaliteamPartnershipBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 
+ * @author:  Maliteam - Consulting <contact@maliteam-consulting.fr>
+ *
+ */
+
+namespace Maliteam\Bundle\PartnershipBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MaliteamPartnershipBundle extends Bundle
+{
+}
